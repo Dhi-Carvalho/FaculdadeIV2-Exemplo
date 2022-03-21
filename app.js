f691f230ae37e90d1b5cb20085591f981555944a
Dhi-Carvalho <dhi.carvalho85@gmail.com>
2022-03-21 18:55:31 -0300

    Exemplo prático do primeiro módulo do curso Beginners da Faculdade IV2

---
+++ app.js
@@ -0,0 +1,5 @@
+var textoQueMuda = document.querySelector('#about p')
+
+textoQueMuda.addEventListener('click', function () {
+  textoQueMuda.innerHTML = '<a href="https://class.faculdadeiv2.com.br/" id="class">Clique aqui e sabia mais</a>'
+})
